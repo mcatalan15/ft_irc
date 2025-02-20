d714061d9ebd534b3c07405d2b85734d2589928b
eferre-m <eva.fmur@gmail.com>
2025-02-20 19:43:50 +0100

    sendmsg mode

--- src/obj/join.d
+++
@@ -1,3 +0,0 @@
-src/obj/join.o: src/join.cpp src/../include/Server.hpp \
- src/../include/Irc.hpp src/../include/Client.hpp \
- src/../include/Channel.hpp
--- src/obj/mode.d
+++
@@ -1,3 +0,0 @@
-src/obj/mode.o: src/mode.cpp src/../include/Server.hpp \
- src/../include/Irc.hpp src/../include/Client.hpp \
- src/../include/Channel.hpp src/../include/Channel.hpp
